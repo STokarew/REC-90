f417c378772aab8b440949b0bdb82c3368164579
сергей токарев <sseregatokarew@gmail.com>
2021-11-18 14:36:58 +0700

    Добавьте файлы проекта.

---
+++ REC-90/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace REC_90
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var day = int.Parse(Console.ReadLine());
+            var days = CultureInfo.CurrentCulture.DateTimeFormat.DayNames;
+            Console.WriteLine(day == 7 ? days[0] : day <= 6 && day >= 1 ? days[day] : "Введенное число не соответствует номеру дня недели");
+            Console.ReadKey();
+        }
+    }
+}
